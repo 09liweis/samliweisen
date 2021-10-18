e07ebc1a7db408c21a84f03740a7997d470c9807
Sam Li <weisen.li@hotmail.com>
2021-10-18 20:22:49 +0800

    add sysdata pi

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,4 +10,24 @@ router.route('/movie/:id').get((req,resp) => {
   resp.json('movie detail '+douban_id);
 });
 
+var os = require('os');
+router.route('/sysdata').post((req,resp)=>{
+  var cpus = os.cpus();
+  for(var i = 0, len = cpus.length; i < len; i++) {
+    console.log("CPU %s:", i);
+    var cpu = cpus[i], total = 0;
+
+    for(var type in cpu.times) {
+      total += cpu.times[type];
+    }
+
+    for(type in cpu.times) {
+      console.log("\t", type, Math.round(100 * cpu.times[type] / total));
+    }
+  }
+  var totalmem = os.totalmem();
+  var freemem = os.freemem();
+  resp.json({totalmem,freemem,cpus});
+});
+
 module.exports = router;

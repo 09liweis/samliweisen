74cd5d8468e588e9c264d17ebfa79900ad7a9e4e
Sam Li <weisen.li@hotmail.com>
2022-01-15 21:22:11 +0800

    fix imdb info with new design

--- server/helpers/imdb.js
+++ server/helpers/imdb.js
@@ -8,15 +8,15 @@ function getImdbUrl(imdb_id) {
 
 exports.getImdbSummary = (imdb_id,cb) => {
   var url = getImdbUrl(imdb_id);
-  sendRequest({url},function(err,{$}) {
+  sendRequest({url},function(err,{$,body}) {
     if (err) {
       return cb(err,null);
     }
     var imdbObj = {};
-    imdbObj.imdb_title = $('.title_wrapper h1').text().trim();
-    imdbObj.imdb_rating = $('span[itemprop="ratingValue"]').text() || 0;
-    imdbObj.imdb_rating_count = $('span[itemprop="ratingCount"]').text() || 0;
-    imdbObj.poster = $('.poster a img').attr('src');
+    imdbObj.imdb_title = $('.TitleHeader__TitleText-sc-1wu6n3d-0').text().trim();
+    imdbObj.imdb_rating = $('.Hero__MetaContainer__Video-sc-kvkd64-4 .AggregateRatingButton__RatingScore-sc-1ll29m0-1').text() || 0;
+    imdbObj.imdb_rating_count = $('.Hero__MetaContainer__Video-sc-kvkd64-4 .AggregateRatingButton__TotalRatingAmount-sc-1ll29m0-3').text() || 0;
+    imdbObj.poster = $('.ipc-image').attr('src');
     cb(null, imdbObj);
   });
 }

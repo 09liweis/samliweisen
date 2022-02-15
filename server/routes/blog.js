98d2399be5fd7fac93974469c001388bdbf67ec2
Sam Li <weisen.li@hotmail.com>
2022-02-15 17:55:22 +0800

    update blog route to use router helper

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const {router} = require('../helpers/router');
 
 const {findList,add,findDetail,update,remove} = require('../controllers/blog.js');
 

e2a5f2ec50c6df15498961c8f5207967f8c8aef3
Sam Li <weisen.li@hotmail.com>
2022-02-16 16:08:10 +0800

    update place route with router

--- server/routes/place.js
+++ server/routes/place.js
@@ -1,5 +1,4 @@
-var express= require('express');
-var router = express.Router();
+const {router} = require('../helpers/router');
 
 const {getList,updateRandom,upsert,getDetail} = require('../controllers/place.js');
 

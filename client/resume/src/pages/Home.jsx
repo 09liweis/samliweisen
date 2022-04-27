94bb96d85b89495b043fafd0c9189ba5d4643254
Sam Li <weisen.li@hotmail.com>
2022-04-27 16:39:22 +0800

    add box style to home

--- client/resume/src/pages/Home.jsx
+++ client/resume/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
+import {Box, BoxTitle, BoxBody} from '../components/style.jsx';
+
 import Experiences from '../components/home/Experiences.jsx';
 import Skills from '../components/home/Skills.jsx';
 import Projects from '../components/home/Projects.jsx';
@@ -10,11 +12,17 @@ import '../css/resume.css';
 const Home = () => {
   const dispatch = useDispatch();
   return (
-    <div className="home">
-      <Experiences />
-      <Projects />
-      <Skills />
-    </div>
+    <Box className="home">
+      <BoxTitle>
+        <i className="boxIcon fa fa-home" aria-hidden="true"></i>
+        <span>Home</span>
+      </BoxTitle>
+      <BoxBody>
+        <Experiences />
+        <Projects />
+        <Skills />
+      </BoxBody>
+    </Box>
   );
 };
 export default Home;

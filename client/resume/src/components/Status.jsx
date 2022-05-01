6c87ac64994193b6f5215641ef7bf0ff4cc84263
Sam Li <weisen.li@hotmail.com>
2022-05-01 10:52:17 +0800

    fix battery level display

--- client/resume/src/components/Status.jsx
+++ client/resume/src/components/Status.jsx
@@ -3,6 +3,9 @@ import React,{useState} from 'react';
 const Status = () => {
   const [level, setLevel] = useState(0);
   const [isCharging, setCharging] = useState(false);
+  const getFormatBatteryLevel = (level) => {
+    return Math.ceil(level*100)+'%';
+  }
   const getBatteryStatus = () => {
     var batteryManager = navigator.getBattery;
     if (typeof batteryManager == 'undefined') {
@@ -11,14 +14,14 @@ const Status = () => {
     navigator.getBattery().then(function(battery){
       var {level, charging} = battery;
       setCharging(charging);
-      setLevel(level);
+      setLevel(getFormatBatteryLevel(level));
       battery.addEventListener('chargingchange',function() {
         var {charging} = battery;
         setCharging(charging);
       });
       battery.addEventListener('levelchange',function() {
         var {level} = battery;
-        setLevel(Math.ceil(level*100)+'%');
+        setLevel(getFormatBatteryLevel(level));
       });
     });
   }

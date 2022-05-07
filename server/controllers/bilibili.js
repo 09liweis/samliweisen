f5fc25449d8b9cc0d6b2500a6121776061740671
Sam Li <weisen.li@hotmail.com>
2022-05-07 11:26:20 +0800

    fix bilibili response

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -5,8 +5,8 @@ exports.getBilibili = (req,resp) => {
   const url = `https://api.bilibili.com/x/space/arc/search?mid=${userId}&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp`;
   sendRequest({url},(err,{body}) => {
     var bVideos = [];
-    if (body.list) {
-      bVideos = body.list.vlist;
+    if (body.data.list) {
+      bVideos = body.data.list.vlist;
     }
     return sendResp(resp,bVideos);
   });

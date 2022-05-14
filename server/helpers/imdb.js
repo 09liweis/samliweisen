847d56dc176562ef8aeb2f98a57c0fbbe1be3f8d
Sam Li <weisen.li@hotmail.com>
2022-05-14 17:10:13 +0800

    fix imdb rating/title/rating_count

--- server/helpers/imdb.js
+++ server/helpers/imdb.js
@@ -13,9 +13,9 @@ exports.getImdbSummary = (imdb_id,cb) => {
       return cb(err,{});
     }
     var imdbObj = {};
-    imdbObj.imdb_title = $('.TitleHeader__TitleText-sc-1wu6n3d-0').text().trim();
-    imdbObj.imdb_rating = $('.Hero__MetaContainer__Video-sc-kvkd64-4 .AggregateRatingButton__RatingScore-sc-1ll29m0-1').text() || 0;
-    imdbObj.imdb_rating_count = $('.Hero__MetaContainer__Video-sc-kvkd64-4 .AggregateRatingButton__TotalRatingAmount-sc-1ll29m0-3').text() || 0;
+    imdbObj.imdb_title = $('.cMYixt .fbOhB').text().trim();
+    imdbObj.imdb_rating = $('.cMYixt .sc-7ab21ed2-1.jGRxWM').text() || 0;
+    imdbObj.imdb_rating_count = $('.cMYixt .dPVcnq').text() || 0;
     imdbObj.poster = $('.ipc-image').attr('src');
     cb(null, imdbObj);
   });

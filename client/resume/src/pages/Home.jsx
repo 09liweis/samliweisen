0813ac57c64573709c63581a0c9a9c355e417bc1
Sam Li <weisen.li@hotmail.com>
2022-07-28 20:52:57 -0400

    update home page title

--- client/resume/src/pages/Home.jsx
+++ client/resume/src/pages/Home.jsx
@@ -4,9 +4,11 @@ import { useDispatch } from 'react-redux';
 import Experiences from '../components/home/Experiences.jsx';
 import Skills from '../components/home/Skills.jsx';
 import Projects from '../components/home/Projects.jsx';
+import {updateDocumentTitle} from '../helpers';
 
 // const Home  = ({characters}) => 
 const Home = () => {
+  updateDocumentTitle('Home');
   const dispatch = useDispatch();
   return (
     <div className="home">

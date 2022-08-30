1b912e86f2db067cb89b16b087a674667ecedf5a
Sam Li <weisen.li@hotmail.com>
2022-08-30 11:32:06 -0400

    update movie component to functional component

--- client/resume/src/components/Movie.jsx
+++ client/resume/src/components/Movie.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
@@ -25,54 +25,50 @@ const doubanIcon = 'https://img3.doubanio.com/f/talion/2f3c0bc0f35b031d4535fd993
 const imdbIcon = 'https://cdn0.iconfinder.com/data/icons/social-media-2091/100/social-31-512.png';
 const errImg = 'https://www.haitioutreach.org/wp-content/uploads/2016/05/404error-graphic.png';
 
-export default class Movie extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  handleErrorImg(e) {
+const Movie = (props) => {
+  const handleErrorImg = (e) => {
     e.target.onerror = null;
     e.target.src = errImg;
   }
-  handleLink(e) {
+  const handleLink = (e) => {
     e.stopPropagation();
     return false;
   }
-  render() {
-    const v = this.props.v;
-    let status;
-    if (v.current_episode == v.episodes) {
-        status = 'done';
-    }
-    if (v.current_episode < v.episodes) {
-        status = 'in_progress';
-        if (v.current_episode == 0) {
-            status = 'not_started';
-        }
-    }
-    const movieHref = "/movie/" + v.id;
-    return (
-      <VisualContainer className='box-shadow border-radius bg-white'>
-        <span className={`visual__status border-radius-top-left text-center ${status}`}>{v.current_episode}/{v.episodes}</span>
-        <ReleaseDate className='border-radius-top-right'>{v.release_date.substr(0,4)}</ReleaseDate>
-        <Link to={movieHref}>
-          <img className="visual__image border-radius-top" src={'https://images.weserv.nl/?url='+v.poster} alt={v.original_title} onError={(e)=>this.handleErrorImg(e)} />
-        </Link>
-        <div className="visual__detail">
-          <VisualTitle className='margin-0 text-color'>{v.title}</VisualTitle>
-          <div className="visual__ratings">
-            <a className="visual__rating" target="_blank" onClick={(e)=>this.handleLink(e)} href={'https://movie.douban.com/subject/' + v.douban_id}>
-              <img className="visual__rating-icon" src={doubanIcon} alt="" />
-              <span className="visual__rating-point">{v.douban_rating}</span>
-            </a>
-            {v.imdb_id ?
-            <a className="visual__rating" target="_blank" onClick={(e)=>this.handleLink(e)} href={'https://www.imdb.com/title/' + v.imdb_id}>
-              <img className="visual__rating-icon" src={imdbIcon} alt="" />
-              <span className="visual__rating-point">{v.imdb_rating}</span>
-            </a>
-            : null}
-          </div>
-        </div>
-      </VisualContainer>
-    );
+  const v = props.v;
+  let status;
+  if (v.current_episode == v.episodes) {
+    status = 'done';
   }
+  if (v.current_episode < v.episodes) {
+    status = 'in_progress';
+    if (v.current_episode == 0) {
+      status = 'not_started';
+    }
+  }
+  const movieHref = "/movie/" + v.id;
+  return (
+    <VisualContainer className='box-shadow border-radius bg-white'>
+      <span className={`visual__status border-radius-top-left text-center ${status}`}>{v.current_episode}/{v.episodes}</span>
+      <ReleaseDate className='border-radius-top-right'>{v.release_date.substr(0,4)}</ReleaseDate>
+      <Link to={movieHref}>
+        <img className="visual__image border-radius-top" src={'https://images.weserv.nl/?url='+v.poster} alt={v.original_title} onError={(e)=>handleErrorImg(e)} />
+      </Link>
+      <div className="visual__detail">
+        <VisualTitle className='margin-0 text-color'>{v.title}</VisualTitle>
+        <div className="visual__ratings">
+          <a className="visual__rating" target="_blank" onClick={(e)=>handleLink(e)} href={'https://movie.douban.com/subject/' + v.douban_id}>
+            <img className="visual__rating-icon" src={doubanIcon} alt="" />
+            <span className="visual__rating-point">{v.douban_rating}</span>
+          </a>
+          {v.imdb_id ?
+          <a className="visual__rating" target="_blank" onClick={(e)=>handleLink(e)} href={'https://www.imdb.com/title/' + v.imdb_id}>
+            <img className="visual__rating-icon" src={imdbIcon} alt="" />
+            <span className="visual__rating-point">{v.imdb_rating}</span>
+          </a>
+          : null}
+        </div>
+      </div>
+    </VisualContainer>
+  );
 }
+export default Movie;

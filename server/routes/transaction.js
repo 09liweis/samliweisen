24402925151355cebcd792a0b5ba5eb8dd5d6b73
Sam Li <weisen.li@hotmail.com>
2022-09-05 13:58:38 -0400

    add transaction getStatistics route

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {verify} = require('../helpers/verifyToken');
 
-const {findList,create,category_list,detail,update,remove} = require('../controllers/transaction.js');
+const {getStatistics,findList,create,category_list,detail,update,remove} = require('../controllers/transaction.js');
 
 router.post('/',verify,findList);
+
+router.post('/statistics',getStatistics);
 
 router.post('/new',verify,create);
 

9a922d647e599bccc9a6b836ac1791aed38d4f89
09liweis <weisen.li@hotmail.com>
2023-04-26 14:12:50 -0400

    refactor: remove angular, what i watched

--- src/app/models/project.ts
+++
@@ -1,9 +0,0 @@
-export class Project {
-	constructor(
-		public _id: string,
-		public title: string,
-		public description: string,
-		public link: string,
-		public features: Array<string>,
-	) {}
-}

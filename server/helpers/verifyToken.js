2a6c83a074b15d59d24f84ad9553bf41dfd3614a
09liweis <weisen.li@hotmail.com>
2023-04-26 14:20:03 -0400

    delete(route): remove news coupon

--- server/helpers/verifyToken.js
+++ server/helpers/verifyToken.js
@@ -1,20 +1,19 @@
 const jwt = require('jsonwebtoken');
-const {token_secret} = require('./constant');
+const { token_secret } = require('./constant');
 
-exports.verify = (req,resp,next) => {
+exports.verify = (req, resp, next) => {
   const token = req.header('auth-token');
   if (!token) {
-    return resp.status(400).json({msg:'Access Denied'});
+    return resp.status(400).json({ msg: 'Access Denied' });
   }
   try {
     const verified = jwt.verify(token, token_secret);
-    console.log('jwt verified',verified);
     req.user = verified;
     return next();
   } catch (error) {
-    resp.status(400).json({msg:'Invalid token'});
+    resp.status(400).json({ msg: 'Invalid token' });
   }
-}
+};
 exports.sign = (data) => {
-  return jwt.sign(data,token_secret);
-}
+  return jwt.sign(data, token_secret);
+};

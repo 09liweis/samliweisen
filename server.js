cba8c9c5a5453176f82ed8edceb6cb7343f34d5c
09liweis <weisen.li@hotmail.com>
2023-04-26 14:25:50 -0400

    fix(server): remove news coupon routes

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 'use strict';
 const DEFAULT_PORT = 8081;
 const express = require('express'),
-path = require('path'),
-app = express(),
-bodyParser = require('body-parser'),
-mongoose = require('mongoose'),
-cors = require("cors"),
-
-indexRoute = require('./server/routes/index'),
-dashboardRoute = require('./server/routes/dashboard'),
-todoRoute = require('./server/routes/todo'),
-transactionRoute = require('./server/routes/transaction'),
-experienceRoute = require('./server/routes/experience'),
-projectRoute = require('./server/routes/project'),
-placeRoute = require('./server/routes/place'),
-blogRoute = require('./server/routes/blog'),
-commentRoute = require('./server/routes/comment'),
-SiteRoute = require('./server/routes/site'),
-newsRoute = require('./server/routes/news'),
-couponsRoute = require('./server/routes/coupons'),
-userRoute = require('./server/routes/user'),
-visualRoute = require('./server/routes/visual'),
-categoryRoute = require('./server/routes/category'),
-
-port = process.env.PORT || DEFAULT_PORT;
+  path = require('path'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  cors = require('cors'),
+  indexRoute = require('./server/routes/index'),
+  dashboardRoute = require('./server/routes/dashboard'),
+  todoRoute = require('./server/routes/todo'),
+  transactionRoute = require('./server/routes/transaction'),
+  experienceRoute = require('./server/routes/experience'),
+  projectRoute = require('./server/routes/project'),
+  placeRoute = require('./server/routes/place'),
+  blogRoute = require('./server/routes/blog'),
+  commentRoute = require('./server/routes/comment'),
+  SiteRoute = require('./server/routes/site'),
+  userRoute = require('./server/routes/user'),
+  visualRoute = require('./server/routes/visual'),
+  categoryRoute = require('./server/routes/category'),
+  port = process.env.PORT || DEFAULT_PORT;
 
 mongoose.Promise = global.Promise;
 
@@ -31,24 +27,26 @@ var dbUrl;
 if (port == DEFAULT_PORT) {
   dbUrl = 'mongodb://localhost:27017/tdlisting';
 } else {
-  dbUrl = 'mongodb+srv://samliweisen:`Qq363404661@samliweisen.3amrq.mongodb.net/heroku_6njptcbp?retryWrites=true&w=majority';
+  dbUrl =
+    'mongodb+srv://samliweisen:`Qq363404661@samliweisen.3amrq.mongodb.net/heroku_6njptcbp?retryWrites=true&w=majority';
   // dbUrl = 'mongodb://heroku_6njptcbp:dg8h3o8v9dpjk1osignqn3ibel@ds125489.mlab.com:25489/heroku_6njptcbp';
 }
-dbUrl = 'mongodb+srv://samliweisen:`Qq363404661@samliweisen.3amrq.mongodb.net/heroku_6njptcbp?retryWrites=true&w=majority';
-mongoose.connect(dbUrl,{ useNewUrlParser: true,useUnifiedTopology:true });
+dbUrl =
+  'mongodb+srv://samliweisen:`Qq363404661@samliweisen.3amrq.mongodb.net/heroku_6njptcbp?retryWrites=true&w=majority';
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 //,{ useNewUrlParser: true, useUnifiedTopology:true,useFindAndModify:true }
 
-mongoose.connection.on('connected', function() {
+mongoose.connection.on('connected', function () {
   console.log('Connected to db');
 });
 
-mongoose.connection.on('error', function() {
+mongoose.connection.on('error', function () {
   console.log('connected fail');
 });
 
-mongoose.connection.on('disconnected', function () {    
+mongoose.connection.on('disconnected', function () {
   console.log('Mongoose connection disconnected');
-}); 
+});
 
 app.use(cors());
 
@@ -67,15 +65,21 @@ app.use(function (req, res, next) {
   ];
   // Website you wish to allow to connect
   if (origins.indexOf(req.headers.origin) > -1) {
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);   
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   }
   if (req.query && req.query.origin && req.query.origin == 'localhost') {
-    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
   }
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,auth-token');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type,auth-token'
+  );
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -85,33 +89,36 @@ app.use(function (req, res, next) {
 app.use('/assets', express.static(path.join(__dirname) + '/assets'));
 app.use('/dashboard', express.static(path.join(__dirname) + '/dashboard'));
 app.use('/resume', express.static(path.join(__dirname) + '/resume'));
-app.use('/what-i-watched', express.static(path.join(__dirname) + '/what-i-watched'));
+app.use(
+  '/what-i-watched',
+  express.static(path.join(__dirname) + '/what-i-watched')
+);
 
-const http = require("http").Server(app);
+const http = require('http').Server(app);
 
 const socketIO = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:3000"
-  }
+    origin: 'http://localhost:3000',
+  },
 });
 
-const {createTodo} = require('./server/controllers/todo.js');
+const { createTodo } = require('./server/controllers/todo.js');
 //👇🏻 Add this before the app.get() block
 socketIO.on('connection', (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
   console.log(socketIO.sockets.sockets.size);
-  socket.on("addTodo", (todo) => {
-      //👇🏻 todo - contains the object from the React app
-      createTodo(todo,(err,newTodo)=>{
-        findTodoList({},(err,todos)=>{
-          socket.emit('getTodos', todos);
-          socket.broadcast.emit('getTodos', todos);
-        });
+  socket.on('addTodo', (todo) => {
+    //👇🏻 todo - contains the object from the React app
+    createTodo(todo, (err, newTodo) => {
+      findTodoList({}, (err, todos) => {
+        socket.emit('getTodos', todos);
+        socket.broadcast.emit('getTodos', todos);
       });
+    });
   });
 
   socket.on('disconnect', () => {
-    socket.disconnect()
+    socket.disconnect();
     console.log('🔥: A user disconnected');
   });
 });
@@ -130,11 +137,11 @@ app.get('/what-i-watched', (req, res) => {
   res.sendFile(path.join(__dirname, 'what-i-watched/what-i-watched.html'));
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', indexRoute);
-app.use('/api/dashboard',dashboardRoute)
+app.use('/api/dashboard', dashboardRoute);
 app.use('/api/todos', todoRoute);
 app.use('/api/transactions', transactionRoute);
 app.use('/api/experiences', experienceRoute);
@@ -143,9 +150,7 @@ app.use('/api/places', placeRoute);
 app.use('/api/blogs', blogRoute);
 app.use('/api/comments', commentRoute);
 app.use('/api/sites', SiteRoute);
-app.use('/api/news', newsRoute);
-app.use('/api/coupons', couponsRoute);
-app.use('/api/user',userRoute);
+app.use('/api/user', userRoute);
 app.use('/api/visuals', visualRoute);
 app.use('/api/category', categoryRoute);
 

efe3e1a2f2c3bffb33e9a31bf1b8e27159d79cfd
Sam Li <weisen.li@hotmail.com>
2023-05-18 10:09:02 -0400

    fix(bilibili): update bilibili api

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -2,7 +2,7 @@ const {sendRequest, sendResp, sendErr} = require('../helpers/request');
 
 exports.getBilibili = (req,resp) => {
   const userId = '19524235';
-  const url = `https://api.bilibili.com/x/space/arc/search?mid=${userId}&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp`;
+  const url = `https://api.bilibili.com/x/space/wbi/arc/search?mid=${userId}&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp`;
   sendRequest({url},(err,{body}) => {
     var bVideos = [];
     if (body.data.list) {

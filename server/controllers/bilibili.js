c44021777172ae11c425597594649d570b9cf1af
Replit user <>
2023-10-13 14:11:53 +0000

    add bilibli api

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -1,14 +1,8 @@
 const { sendRequest, sendResp, sendErr } = require('../helpers/request');
+const biliAPI = require('bili-api');
 
-exports.getBilibili = (req, resp) => {
+exports.getBilibili = async (req, resp) => {
   const userId = '19524235';
-  const url = `https://api.bilibili.com/x/space/wbi/arc/search?mid=${userId}&ps=30&tid=0&pn=1&keyword=&order=pubdate&platform=web&web_location=1550101&order_avoided=true&w_rid=f528bdb22436fc1377cc3100038122aa&wts=1686749932`;
-  sendRequest({ url }, (err, { body }) => {
-    console.log(body);
-    var bVideos = [];
-    if (body.data?.list) {
-      bVideos = body.data?.list?.vlist;
-    }
-    return sendResp(resp, bVideos);
-  });
+  let up = await biliAPI({ mid: userId }, ['allVideos'])
+  return sendResp(resp, up);
 }

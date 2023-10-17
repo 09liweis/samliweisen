e8b7943c93f17d0a9dee8e84298c05eadc8d8f4a
Replit user <>
2023-10-17 17:27:09 +0000

    add bilibili dynamic

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -3,6 +3,6 @@ const biliAPI = require('bili-api');
 
 exports.getBilibili = async (req, resp) => {
   const userId = '19524235';
-  let up = await biliAPI({ mid: userId }, ['allVideos'])
+  let up = await biliAPI({ mid: userId }, ['allVideos','dynamics'])
   return sendResp(resp, up);
 }

1ef292f498798d349875ca49dbe5ed7a6767c115
09liweis <weisen.li@hotmail.com>
2023-11-02 13:08:21 +0000

    use async await to refactor experiences find list function

--- server/controllers/experience.js
+++ server/controllers/experience.js
@@ -1,10 +1,12 @@
 var Experience = require('../models/experience');
 
-exports.findList = (req, resp) => {
-  Experience.find({}).sort('-start_date').exec((err, experiences) => {
-    handleError(resp, err);
+exports.findList = async (req, resp) => {
+  try {
+    const experiences = await Experience.find({}).sort('-start_date')
     resp.json(experiences);
-  });
+  } catch (err) {
+    resp.json({ err });
+  }
 };
 
 exports.findDetail = (req, res) => {
@@ -26,7 +28,7 @@ exports.create = (req, res) => {
 exports.update = (req, res) => {
   let updateExperience = req.body;
   updateExperience.update_at = new Date();
-  Experience.findOneAndUpdate({_id: req.params.id}, updateExperience, {upsert: true}, (err, experience) => {
+  Experience.findOneAndUpdate({ _id: req.params.id }, updateExperience, { upsert: true }, (err, experience) => {
     handleError(res, err);
     res.json(experience);
   });

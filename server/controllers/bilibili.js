d23d97f98ec04c30b013e1ece4544f88c53bd23d
Replit user <>
2023-11-03 16:34:32 +0000

    add try catch to bilibili request

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -1,8 +1,12 @@
-const { sendRequest, sendResp, sendErr } = require('../helpers/request');
+const { sendResp, sendErr } = require('../helpers/request');
 const biliAPI = require('bili-api');
 
 exports.getBilibili = async (req, resp) => {
-  const userId = '19524235';
-  let up = await biliAPI({ mid: userId }, ['allVideos','dynamics'])
-  return sendResp(resp, up);
+  try {
+    const userId = '19524235';
+    let up = await biliAPI({ mid: userId }, ['allVideos', 'dynamics'])
+    return sendResp(resp, up);
+  } catch (err) {
+    return sendErr(resp, { err: err.toString() });
+  }
 }

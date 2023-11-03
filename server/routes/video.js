11ac795e2a886746040d63e04bf6e8a376cda45b
Replit user <>
2023-11-03 16:35:47 +0000

    remove maoyan import from video.js

--- server/routes/video.js
+++ server/routes/video.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const { getBilibili } = require('../controllers/bilibili.js');
-const { getMaoyan } = require('../controllers/maoyan.js');
 
 router.route('/bilibili').get(getBilibili);
 

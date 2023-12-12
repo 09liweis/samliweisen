888fed27771026c0cc8e82090e50b80b1a49b73a
09liweis <weisen.li@hotmail.com>
2023-12-12 15:48:24 +0000

    set gross in currency format

--- server/controllers/imdb.js
+++ server/controllers/imdb.js
@@ -2,6 +2,16 @@ const { sendRequest, sendResp, sendErr } = require('../helpers/request');
 
 const IMDB_BOXOFFICE = 'https://www.imdb.com/chart/boxoffice';
 
+function getCurrencyFormat(value) {
+  var suffixes = ['', 'K', 'M', 'B', 'T'];
+  var suffixNum = Math.floor(('' + value).length / 3);
+  var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(2));
+  if (shortValue % 1 != 0) {
+    shortValue = shortValue.toFixed(1);
+  }
+  return shortValue + suffixes[suffixNum];
+}
+
 exports.getImdbBoxOffice = (req, resp) => {
   sendRequest({ url: IMDB_BOXOFFICE }, (err, { $ }) => {
     if (err) {
@@ -14,7 +24,7 @@ exports.getImdbBoxOffice = (req, resp) => {
     const jsonLdInfo = $('script[type="application/json"]').text();
     const json = JSON.parse(jsonLdInfo);
     const movies = json?.props?.pageProps?.pageData?.topGrossingReleases?.edges;
-    boxOffice.movies = movies.map(({node})=>{
+    boxOffice.movies = movies.map(({ node }) => {
       const movie = node.release.titles[0];
       return {
         title: movie.titleText.text,
@@ -23,8 +33,8 @@ exports.getImdbBoxOffice = (req, resp) => {
         imdb_rating: movie.ratingsSummary.aggregateRating,
         vote_count: movie.ratingsSummary.voteCount,
         summary: movie.plot.plotText.plainText,
-        totalGross: movie.lifetimeGross.total.amount,
-        currentGross: node.gross.total.amount
+        totalGross: getCurrencyFormat(movie.lifetimeGross.total.amount),
+        currentGross: getCurrencyFormat(node.gross.total.amount)
       }
     });
     return sendResp(resp, boxOffice);

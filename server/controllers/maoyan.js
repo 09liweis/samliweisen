7997392ef5a82f7af5eb6d3e952dccad777cf891
09liweis <weisen.li@hotmail.com>
2023-12-19 16:04:57 +0000

    handle err for maoyan

--- server/controllers/maoyan.js
+++ server/controllers/maoyan.js
@@ -1,9 +1,10 @@
 //Blocked by maoyan
-const {sendRequest, sendResp, sendErr} = require('../helpers/request');
+const { sendRequest, sendResp, sendErr } = require('../helpers/request');
 
-exports.getMaoyan = (req,resp) => {
+exports.getMaoyan = (req, resp) => {
   const url = 'https://piaofang.maoyan.com/dashboard-ajax';
-  sendRequest({url},(err,{body}) => {
-    return sendResp(resp,body);
+  sendRequest({ url }, (err, { body }) => {
+    if (err) return sendErr(resp, err);
+    return sendResp(resp, body);
   });
 }

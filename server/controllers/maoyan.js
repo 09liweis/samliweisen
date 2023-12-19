303498155f41b3231d7f29c23797dd923885feb4
09liweis <weisen.li@hotmail.com>
2023-12-19 18:20:22 +0000

    format maoyan boxoffice data

--- server/controllers/maoyan.js
+++ server/controllers/maoyan.js
@@ -4,7 +4,13 @@ const { sendRequest, sendResp, sendErr } = require('../helpers/request');
 exports.getMaoyan = (req, resp) => {
   const url = 'https://piaofang.maoyan.com/dashboard-ajax';
   sendRequest({ url }, (err, { body }) => {
-    if (err) return sendErr(resp, err);
-    return sendResp(resp, body);
+    if (err) return sendErr(resp, { err: err.toString() });
+    const movies = body.movieList.data.list.map((movie) => {
+      return {
+        title: movie.movieInfo.movieName,
+        totalGross: movie.sumBoxDesc
+      };
+    })
+    return sendResp(resp, { movies });
   });
 }

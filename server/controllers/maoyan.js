1621e66de35e408aa50334575f1eee7ad9b889ea
09liweis <weisen.li@hotmail.com>
2023-12-29 19:05:59 +0000

    handle error for maoyan

--- server/controllers/maoyan.js
+++ server/controllers/maoyan.js
@@ -1,11 +1,12 @@
-//Blocked by maoyan
 const { sendRequest, sendResp, sendErr } = require('../helpers/request');
 
 exports.getMaoyan = (req, resp) => {
   const url = 'https://piaofang.maoyan.com/dashboard-ajax';
   sendRequest({ url }, (err, { body }) => {
     if (err) return sendErr(resp, { err: err.toString() });
-    const movies = body.movieList.data.list.map((movie) => {
+    const maoyanMovies = body?.movieList?.data?.list;
+    if (!maoyanMovies) return sendErr(resp, { err: 'No Maoyan movies found' });
+    const movies = maoyanMovies.map((movie) => {
       return {
         title: movie.movieInfo.movieName,
         totalGross: movie.sumBoxDesc

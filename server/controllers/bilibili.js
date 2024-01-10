8197a530942293ec47c8dbb90fc06cc4c422c609
09liweis <weisen.li@hotmail.com>
2024-01-10 14:55:03 +0000

    format bilibili, update package.json version

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -1,12 +1,12 @@
-const { sendResp, sendErr } = require('../helpers/request');
-const biliAPI = require('bili-api');
+const { sendResp, sendErr } = require("../helpers/request");
+const biliAPI = require("bili-api");
 
 exports.getBilibili = async (req, resp) => {
   try {
-    const userId = '19524235';
-    let up = await biliAPI({ mid: userId }, ['allVideos', 'dynamics']);
+    const userId = "19524235";
+    let up = await biliAPI({ mid: userId }, ["allVideos", "dynamics"]);
     return sendResp(resp, up);
   } catch (err) {
     return sendErr(resp, { err: err.toString() });
   }
-}
+};

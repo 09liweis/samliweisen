9cdca77f3e1f88863994300599667c24539fff8c
09liweis <weisen.li@hotmail.com>
2024-01-25 17:49:24 +0000

    format transaction controller

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -1,6 +1,6 @@
-var Transaction = require('../models/transaction');
-var Place = require('../models/place');
-const { sendRequest, sendResp, sendErr } = require('../helpers/request');
+var Transaction = require("../models/transaction");
+var Place = require("../models/place");
+const { sendRequest, sendResp, sendErr } = require("../helpers/request");
 
 exports.getStatistics = (req, resp) => {
   let { date } = req.body;
@@ -9,35 +9,43 @@ exports.getStatistics = (req, resp) => {
   if (!date) {
     const now = new Date();
     const month = now.getMonth() + 1;
-    date = `${now.getFullYear()}-${month < 10 ? '0' + month : month}`;
+    date = `${now.getFullYear()}-${month < 10 ? "0" + month : month}`;
   }
-  filter.date = new RegExp(date, 'i');
+  filter.date = new RegExp(date, "i");
   statistics.date = date;
-  Transaction.find(filter, '_id title price date category').populate('place').sort('-date').exec((err, transactions) => {
-    statistics.categoryPrice = {}
-    if (transactions.length == 0) {
-      return sendResp(resp, statistics);
-    }
-    handleError(resp, err);
-    transactions.forEach((transaction) => {
-      let { category, price } = transaction;
-      price = Math.abs(price);
-      statistics.total += price;
-      if (statistics.categoryPrice[category]) {
-        statistics.categoryPrice[category].total += price;
-        statistics.categoryPrice[category].items.push(transaction);
-      } else {
-        statistics.categoryPrice[category] = { total: price, items: [transaction] };
+  Transaction.find(filter, "_id title price date category")
+    .populate("place")
+    .sort("-date")
+    .exec((err, transactions) => {
+      statistics.categoryPrice = {};
+      if (transactions.length == 0) {
+        return sendResp(resp, statistics);
       }
+      if (err) {
+        return sendErr(resp, { err: err.toString() });
+      }
+      transactions.forEach((transaction) => {
+        let { category, price } = transaction;
+        price = Math.abs(price);
+        statistics.total += price;
+        if (statistics.categoryPrice[category]) {
+          statistics.categoryPrice[category].total += price;
+          statistics.categoryPrice[category].items.push(transaction);
+        } else {
+          statistics.categoryPrice[category] = {
+            total: price,
+            items: [transaction],
+          };
+        }
+      });
+      sendResp(resp, statistics);
     });
-    sendResp(resp, statistics);
-  });
-}
+};
 
 exports.findList = (req, resp) => {
   const user = req.user;
   if (!user) {
-    return resp.status(400).json({ msg: 'Login Required' });
+    return resp.status(400).json({ msg: "Login Required" });
   }
   let filter = { uid: user._id };
   const { category, date, place_id, limit, page, uid } = req.body;
@@ -46,7 +54,7 @@ exports.findList = (req, resp) => {
   }
   let opt = { limit: 10 };
   if (limit) {
-    if (limit != 'all') {
+    if (limit != "all") {
       opt.limit = parseInt(limit);
     } else {
       delete opt.limit;
@@ -56,8 +64,8 @@ exports.findList = (req, resp) => {
     opt.skip = parseInt(page) * opt.limit;
   }
   if (category) {
-    var inCate = category['$in'];
-    var ninCate = category['$nin'];
+    var inCate = category["$in"];
+    var ninCate = category["$nin"];
     if ((inCate && inCate.length > 0) || (ninCate && ninCate.length > 0)) {
       filter.category = category;
     }
@@ -66,16 +74,19 @@ exports.findList = (req, resp) => {
     filter.place = place_id;
   }
   if (date) {
-    filter.date = new RegExp(date, 'i');
+    filter.date = new RegExp(date, "i");
   }
-  Transaction.find(filter, '_id title price date category', opt).populate('place', '_id name address lat lng icon').sort('-date').exec((err, transactions) => {
-    handleError(resp, err);
-    sendResp(resp, transactions);
-  });
+  Transaction.find(filter, "_id title price date category", opt)
+    .populate("place", "_id name address lat lng icon")
+    .sort("-date")
+    .exec((err, transactions) => {
+      handleError(resp, err);
+      sendResp(resp, transactions);
+    });
 };
 
 exports.category_list = (req, resp) => {
-  Transaction.distinct('category', (err, categories) => {
+  Transaction.distinct("category", (err, categories) => {
     handleError(resp, err);
     sendResp(resp, categories);
   });
@@ -83,7 +94,7 @@ exports.category_list = (req, resp) => {
 upsertTransaction = async (req, resp) => {
   const user = req.user;
   if (!user) {
-    return resp.status(400).json({ msg: 'Login Required' });
+    return resp.status(400).json({ msg: "Login Required" });
   }
   const { _id, price, date, category, place, title, uid } = req.body;
   transactionData = {
@@ -91,13 +102,13 @@ upsertTransaction = async (req, resp) => {
     price,
     date,
     category,
-    title
+    title,
   };
   if (uid) {
     transactionData.uid = uid;
   }
   let p;
-  if (typeof place != 'undefined') {
+  if (typeof place != "undefined") {
     p = await Place.findOne({ place_id: place.place_id });
     if (!p) {
       p = Place(place);
@@ -115,20 +126,25 @@ upsertTransaction = async (req, resp) => {
   if (_id) {
     transaction = transactionData;
     transaction.update_at = new Date();
-    Transaction.findOneAndUpdate({ _id }, transaction, { returnNewDocument: true, upsert: true }, (err, t) => {
-      console.error(err);
-      t.place = p;
-      return sendResp(resp, t);
-    });
+    Transaction.findOneAndUpdate(
+      { _id },
+      transaction,
+      { returnNewDocument: true, upsert: true },
+      (err, t) => {
+        console.error(err);
+        t.place = p;
+        return sendResp(resp, t);
+      },
+    );
   } else {
     transaction = new Transaction(transactionData);
-    transaction.save(function(err, t) {
+    transaction.save(function (err, t) {
       handleError(resp, err);
       t.place = p;
       return sendResp(resp, t);
     });
   }
-}
+};
 exports.create = (req, resp) => {
   upsertTransaction(req, resp);
 };
@@ -137,9 +153,12 @@ exports.update = (req, resp) => {
   upsertTransaction(req, resp);
 };
 
-exports.detail = async function(req, resp) {
+exports.detail = async function (req, resp) {
   const id = req.params.id;
-  const t = await Transaction.findById(id, 'title price date category').populate('place', '_id place_id name address lat lng');
+  const t = await Transaction.findById(
+    id,
+    "title price date category",
+  ).populate("place", "_id place_id name address lat lng");
   return sendResp(resp, t);
 };
 
@@ -147,11 +166,11 @@ exports.remove = (req, resp) => {
   //Delete transaction
   const user = req.user;
   if (!user) {
-    return resp.status(400).json({ msg: 'Login Required' });
+    return resp.status(400).json({ msg: "Login Required" });
   }
   Transaction.remove({ _id: req.params.id }, (err) => {
     handleError(resp, err);
-    resp.status(200).json({ ok: 1, msg: 'Transaction Deleted' });
+    resp.status(200).json({ ok: 1, msg: "Transaction Deleted" });
   });
 };
 

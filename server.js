cd8cfa5955e3ae36cf97c124a23ac805ace18db1
Sam Li <weisen.li@hotmail.com>
2024-03-05 18:16:02 +0000

    encapsulate expressjs

--- server.js
+++ server.js
@@ -23,97 +23,11 @@ mongoose.connection.on("disconnected", function () {
   //TODO: add email notification
 });
 
-const express = require("express"),
-  path = require("path"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  cors = require("cors"),
-  indexRoute = require("./server/routes/index"),
-  dashboardRoute = require("./server/routes/dashboard"),
-  todoRoute = require("./server/routes/todo"),
-  transactionRoute = require("./server/routes/transaction"),
-  experienceRoute = require("./server/routes/experience"),
-  projectRoute = require("./server/routes/project"),
-  placeRoute = require("./server/routes/place"),
-  blogRoute = require("./server/routes/blog"),
-  commentRoute = require("./server/routes/comment"),
-  SiteRoute = require("./server/routes/site"),
-  userRoute = require("./server/routes/user"),
-  visualRoute = require("./server/routes/visual"),
-  videoRoute = require("./server/routes/video"),
-  categoryRoute = require("./server/routes/category"),
-  roomRoute = require("./server/routes/room"),
-  contactRoute = require("./server/routes/contact");
+const ExpressJs = require("./server/express");
+const express = new ExpressJs();
+express.init();
 
-app.use(cors());
-
-const getPathName = (dir) => {
-  return express.static(path.join(__dirname) + "/" + dir);
-};
-
-app.use(function (req, resp, next) {
-  var host = req.headers.host;
-  const origins = [
-    "https://samliweisen.onrender.com/",
-    "https://dashboard.09liweis.repl.co/",
-    "http://localhost:4200",
-    "http://localhost:8081",
-    "http://localhost:3000",
-    "https://what-sam-watched.onrender.com/",
-    "https://samliweisen.cyclic.app/",
-  ];
-  // Website you wish to allow to connect
-  if (origins.indexOf(req.headers.origin) > -1) {
-    resp.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-  }
-  if (req.query && req.query.origin && req.query.origin == "localhost") {
-    resp.setHeader("Access-Control-Allow-Origin", "*");
-  }
-  // Request methods you wish to allow
-  resp.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-  );
-  // Request headers you wish to allow
-  resp.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type,auth-token",
-  );
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  resp.setHeader("Access-Control-Allow-Credentials", true);
-  // Pass to next layer of middleware
-  next();
-});
-
-app.use("/assets", getPathName("assets"));
-app.use("/resume", getPathName("resume"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "resume/resume.html"));
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use("/api", indexRoute);
-app.use("/api/dashboard", dashboardRoute);
-app.use("/api/videos", videoRoute);
-app.use("/api/movies", visualRoute);
-app.use("/api/todos", todoRoute);
-app.use("/api/transactions", transactionRoute);
-app.use("/api/experiences", experienceRoute);
-app.use("/api/projects", projectRoute);
-app.use("/api/blogs", blogRoute);
-app.use("/api/comments", commentRoute);
-app.use("/api/sites", SiteRoute);
-app.use("/api/user", userRoute);
-app.use("/api/category", categoryRoute);
-app.use("/api/places", placeRoute);
-app.use("/api/rooms", roomRoute);
-app.use("/api/contacts", contactRoute);
-
-const http = require("http").Server(app);
+const http = require("http").Server(express.getApp());
 
 const socketIO = require("socket.io")(http, {
   cors: {
--- server/express.js
+++ server/express.js
@@ -1,12 +1,102 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const indexRoute = require("./routes/index"),
+  dashboardRoute = require("./routes/dashboard"),
+  todoRoute = require("./routes/todo"),
+  transactionRoute = require("./routes/transaction"),
+  experienceRoute = require("./routes/experience"),
+  projectRoute = require("./routes/project"),
+  placeRoute = require("./routes/place"),
+  blogRoute = require("./routes/blog"),
+  commentRoute = require("./routes/comment"),
+  SiteRoute = require("./routes/site"),
+  userRoute = require("./routes/user"),
+  visualRoute = require("./routes/visual"),
+  videoRoute = require("./routes/video"),
+  categoryRoute = require("./routes/category"),
+  roomRoute = require("./routes/room"),
+  contactRoute = require("./routes/contact");
+
+const getPathName = (dir) => {
+  return express.static(path.join(__dirname) + "/" + dir);
+};
 
 class ExpressJs {
   constructor() {
-    this.app = express;
+    this.app = express();
+    this.init();
   }
-  setRoute(route) {
-    this.app.use(route);
+  getApp() {
+    return this.app;
+  }
+  init() {
+    this.app.use(cors());
+
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+
+    this.app.use("/assets", getPathName("../assets"));
+    this.app.use("/resume", getPathName("../resume"));
+
+    this.app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "../resume/resume.html"));
+    });
+
+    this.app.use("/api", indexRoute);
+    this.app.use("/api/dashboard", dashboardRoute);
+    this.app.use("/api/videos", videoRoute);
+    this.app.use("/api/movies", visualRoute);
+    this.app.use("/api/todos", todoRoute);
+    this.app.use("/api/transactions", transactionRoute);
+    this.app.use("/api/experiences", experienceRoute);
+    this.app.use("/api/projects", projectRoute);
+    this.app.use("/api/blogs", blogRoute);
+    this.app.use("/api/comments", commentRoute);
+    this.app.use("/api/sites", SiteRoute);
+    this.app.use("/api/user", userRoute);
+    this.app.use("/api/category", categoryRoute);
+    this.app.use("/api/places", placeRoute);
+    this.app.use("/api/rooms", roomRoute);
+    this.app.use("/api/contacts", contactRoute);
+
+    this.app.use(function (req, resp, next) {
+      var host = req.headers.host;
+      const origins = [
+        "https://samliweisen.onrender.com/",
+        "https://dashboard.09liweis.repl.co/",
+        "http://localhost:4200",
+        "http://localhost:8081",
+        "http://localhost:3000",
+        "https://what-sam-watched.onrender.com/",
+        "https://samliweisen.cyclic.app/",
+      ];
+      // Website you wish to allow to connect
+      if (origins.indexOf(req.headers.origin) > -1) {
+        resp.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+      }
+      if (req.query && req.query.origin && req.query.origin == "localhost") {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+      }
+      // Request methods you wish to allow
+      resp.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      );
+      // Request headers you wish to allow
+      resp.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type,auth-token",
+      );
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      resp.setHeader("Access-Control-Allow-Credentials", true);
+      // Pass to next layer of middleware
+      next();
+    });
   }
 }
 
-export default ExpressJs;
+module.exports = ExpressJs;

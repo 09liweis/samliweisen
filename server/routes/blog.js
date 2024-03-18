fa6f3589afe508a0814ad1b5367ee9ee3794e87b
Sam Li <weisen.li@hotmail.com>
2024-03-18 01:19:10 +0000

    add verify to blog upsert, delete

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { verify } = require("../helpers/verifyToken");
 
-const {findList,add,findDetail,update,remove} = require('../controllers/blog.js');
+const {
+  findList,
+  add,
+  findDetail,
+  update,
+  remove,
+} = require("../controllers/blog.js");
 
-router.route('/')
-.get(findList)
-.post(add);
+router.route("/").get(findList).post(verify, add);
 
-router.route('/:id')
-.get(findDetail)
-.put(update)
-.delete(remove);
+router.route("/:id").get(findDetail).put(verify, update).delete(verify, remove);
 
 module.exports = router;

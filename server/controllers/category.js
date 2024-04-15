a35f9e0c374635d3bb2e0db196e97378eb0ab0cf
Sam Li <weisen.li@hotmail.com>
2024-04-15 20:17:03 +0000

    fix catgory api

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,38 +1,41 @@
-const Category = require('../models/category');
-const Transaction = require('../models/transaction');
+const Category = require("../models/category");
+const Transaction = require("../models/transaction");
 
 exports.category_list = (req, resp) => {
-  Category.find().exec((err, categories) => {
-    handleError(resp, err);
-    resp.json(categories);
-  });
+  Category.find()
+    .then((categories) => {
+      resp.json(categories);
+    })
+    .catch((err) => {
+      handleError(resp, err);
+    });
 };
-exports.category_detail = (req,res)=>{
+exports.category_detail = (req, res) => {
   res.json([]);
 };
-exports.category_import = (req,res)=>{
-  console.log('Start importing at '+new Date());
-  Transaction.distinct('category', (err, categories) => {
-    if (categories && categories.length > 0){
-      Category.remove({},(err,ret)=>{
+exports.category_import = (req, res) => {
+  console.log("Start importing at " + new Date());
+  Transaction.distinct("category", (err, categories) => {
+    if (categories && categories.length > 0) {
+      Category.remove({}, (err, ret) => {
         let count = categories.length;
         function saveCat() {
           const name = categories.pop();
-          let category = new Category({name});
-          category.save((err,saved)=>{
+          let category = new Category({ name });
+          category.save((err, saved) => {
             if (--count) {
               saveCat();
             } else {
-              console.log('Finish importing at '+new Date())
-              res.json('ok');
-            };
+              console.log("Finish importing at " + new Date());
+              res.json("ok");
+            }
           });
         }
         saveCat();
       });
     }
   });
-}
+};
 function handleError(res, err) {
   if (err) {
     res.send(err);

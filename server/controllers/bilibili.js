5f4a8f4426521653a36ccbcf8789579aa1bfd193
Sam Li <weisen.li@hotmail.com>
2024-05-14 17:07:41 +0000

    warning if not MONGODB_URL

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -1,11 +1,18 @@
-const { sendResp, sendErr } = require("../helpers/request");
+const { sendResp, sendErr, sendRequest } = require("../helpers/request");
 const biliAPI = require("bili-api");
 
 exports.getBilibili = async (req, resp) => {
   try {
     const userId = "19524235";
-    let up = await biliAPI({ mid: userId }, ["dynamics"]);
-    return sendResp(resp, up);
+    // let up = await biliAPI({ mid: userId }, ["dynamicsRaw"]);
+    sendRequest(
+      {
+        url: "https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid=19524235",
+      },
+      (err, { body }) => {
+        return sendResp(resp, body);
+      },
+    );
   } catch (err) {
     return sendErr(resp, { err: err.toString() });
   }

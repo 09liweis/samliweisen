49e03cdde8304f2ff58511069f6bdae7eeb85fea
Sam Li <weisen.li@hotmail.com>
2024-05-16 18:47:41 +0000

    add auth to todo api

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,18 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { verify } = require("../helpers/verifyToken");
 
-const {findList,create,findDetail,update,remove,updateStep} = require('../controllers/todo.js');
+const {
+  findList,
+  create,
+  findDetail,
+  update,
+  remove,
+  updateStep,
+} = require("../controllers/todo.js");
 
-router.route('/')
-.get(findList)
-.post(create);
+router.route("/").get(findList).post(verify, create);
 
-router.route('/:id')
-.get(findDetail)
-.put(update)
-.delete(remove);
+router.route("/:id").get(findDetail).put(verify, update).delete(verify, remove);
 
-router.route('/:id/update_step')
-.post(updateStep)
+router.route("/:id/update_step").post(verify, updateStep);
 
 module.exports = router;

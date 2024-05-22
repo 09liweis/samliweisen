7f1fb176b2492baab894420f6f7cec9f67df3d49
Sam Li <weisen.li@hotmail.com>
2024-05-22 14:28:00 +0000

    add method doc to getNodeChildText

--- server/helpers/parseSelector.js
+++ server/helpers/parseSelector.js
@@ -19,6 +19,12 @@ class ParseSelector {
     return node.find(tag);
   }
 
+  /**
+   * return text of give node child element tag
+   * @param {object} node
+   * @param {string} tag
+   * @return {string}
+   */
   getNodeChildText(node, tag) {
     return node.find(tag).text();
   }

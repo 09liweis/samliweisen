8a38fd9ccab8d4c42624c95258652339a87e6b45
Sam Li <weisen.li@hotmail.com>
2024-07-03 18:22:33 +0000

    update room fields

--- server/models/room.js
+++ server/models/room.js
@@ -1,26 +1,27 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var RoomSchema = new Schema({
   nm: String,
-  isAvailable: Boolean,
-  startDate: String,
-  endDate: String,
-  cmts: String,//comments
+  isAvailable: {
+    type: Boolean,
+    defaut: true,
+  },
+  cmts: String, //comments
   lastChecked: {
-    type: Date
+    type: Date,
   },
   ts: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   mt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
 });
 
-RoomSchema.pre('save', (next) => {
+RoomSchema.pre("save", (next) => {
   const currentDate = new Date();
   this.mt = currentDate;
   if (!this.ts) {
@@ -29,4 +30,4 @@ RoomSchema.pre('save', (next) => {
   next();
 });
 
-module.exports = mongoose.model('Room', RoomSchema);
+module.exports = mongoose.model("Room", RoomSchema);

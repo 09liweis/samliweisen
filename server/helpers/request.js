0952244860b3e2210656b340a446fd287069b90a
Sam Li <weisen.li@hotmail.com>
2024-11-27 19:18:07 +0000

    update lang

--- server/helpers/request.js
+++ server/helpers/request.js
@@ -12,7 +12,7 @@ const getCheerio = (body) => {
 
 const headers = {
   "Ocp-Apim-Subscription-Key": "dcdac5601d864addbc2675a2e96cb1f8", //for cineplex
-  "Accept-Language": "zh-CN,zh;q=0.8",
+  "Accept-Language": "en-CA,en;q=0.8",
   "Accept-Charset": "utf-8, iso-8859-1;q=0.5",
   "User-Agent":
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",

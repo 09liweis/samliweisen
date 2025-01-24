3f124c27e46188fce0404bd3e54b077f2bdcaa4d
Sam Li <weisen.li@hotmail.com>
2025-01-24 15:01:37 +0000

    add verify to transaction statistic

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -1,21 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {verify} = require('../helpers/verifyToken');
+const { verify } = require("../helpers/verifyToken");
 
-const {getStatistics,findList,create,category_list,detail,update,remove} = require('../controllers/transaction.js');
+const {
+  getStatistics,
+  findList,
+  create,
+  category_list,
+  detail,
+  update,
+  remove,
+} = require("../controllers/transaction.js");
 
-router.post('/',verify,findList);
+router.post("/", verify, findList);
 
-router.post('/statistics',getStatistics);
+router.post("/statistics", verify, getStatistics);
 
-router.post('/new',verify,create);
+router.post("/new", verify, create);
 
-router.route('/categories')
-.get(category_list);
+router.route("/categories").get(category_list);
 
-router.route('/:id')
-.post(detail)
-.put(verify,update)
-.delete(verify,remove);
+router.route("/:id").post(detail).put(verify, update).delete(verify, remove);
 
 module.exports = router;

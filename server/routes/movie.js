c402c91ba316824f428dbba638f580d895e54dc3
Sam Li <weisen.li@hotmail.com>
2026-01-29 15:58:19 +0000

    remove douban movie detail route

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -23,23 +23,7 @@ const {
   getCast,
 } = require("../controllers/douban.js");
 
-router.route("/douban/:douban_id/casts").get(getCelebrities);
-
-router.route("/cast/:cast_id").get(getCast);
-
 router.route("/douban/:douban_id").get(getSummary);
-
-router.route("/douban/:douban_id/videos").get(getVideos);
-
-router.route("/video/:video_id").get(getVideo);
-
-router.route("/douban/:douban_id/photos").get(getPhotos);
-
-router.route("/photo/:photo_id").get(getPhotoDetail);
-
-router.route("/douban/:douban_id/comments").get(getComments);
-
-router.route("/douban/:douban_id/reviews").get(getReviews);
 
 router.route("/random").get(getRandomMovie);
 

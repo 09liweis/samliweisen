f01094feb84a4964e7f994fd94af6e2f482f08c8
Sam Li <weisen.li@hotmail.com>
2026-03-09 19:10:05 +0000

    update bilibili controller

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -5,14 +5,12 @@ exports.getBilibili = async (req, resp) => {
   try {
     const userId = "19524235";
     // let up = await biliAPI({ mid: userId }, ["dynamicsRaw"]);
-    sendRequest(
+    const bilibiliResp = await sendRequest(
       {
         url: "https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid=19524235",
-      },
-      (err, { body }) => {
-        return sendResp(resp, body);
-      },
+      }
     );
+    return sendResp(resp, bilibiliResp);
   } catch (err) {
     return sendErr(resp, { err: err.toString() });
   }
